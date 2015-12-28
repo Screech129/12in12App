17b2cf0d3b7910b90a64efba6627af52ec4fcb75
Screech129 <Dsilver2989@gmail.com>
2015-12-28 10:11:28 -0600

    Initial check in

---
+++ 12in12App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_12in12App.MvcApplication" Language="C#" %>
---
+++ 12in12App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_12in12App.Startup))]
+namespace _12in12App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
